0f40714fd0d29d4d9eed10eb9386082609a38a36
Vinayak Prakash Sutar <dvinayaksutar@gmail.com>
2025-04-20 11:01:16 +0530

    updated admin.html, main.sql, and layout.css

--- main.sql
+++ main.sql
@@ -1,1 +1,6 @@
-SELECT a.*, CASE WHEN EXISTS (SELECT 1 FROM Menus WHERE PMenuId = a.MenuId) THEN 1 ELSE 0 END AS haschild FROM Menus AS a WHERE ISNULL(a.PMenuId, 0) = 0
+SELECT a.*, CASE WHEN EXISTS (SELECT 1 FROM Menus WHERE PMenuId = a.MenuId) THEN 1 ELSE 0 END AS haschild 
+FROM Adhvik_Core_API.dbo.Menus AS a WHERE ISNULL(a.PMenuId, 0) = 0
+SELECT a.*, CASE WHEN EXISTS (SELECT 1 FROM Menus WHERE PMenuId = a.MenuId) THEN 1 ELSE 0 END AS haschild 
+FROM Adhvik_Core_API.dbo.Menus AS a WHERE a.PMenuId = 22
+SELECT * FROM Adhvik_Core_API.dbo.MenusForClient WHERE ClientId='MaharashtraNews1'
+SELECT * FROM AccountUserAccess
