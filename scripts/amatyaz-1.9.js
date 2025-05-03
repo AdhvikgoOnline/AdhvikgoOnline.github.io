b31b0e605cee7b1293687e890a504efe5dffa944
Vinayak Prakash Sutar <dvinayaksutar@gmail.com>
2025-05-03 07:52:15 +0530

    unnessary / commented code removed

--- scripts/amatyaz-1.9.js
+++ scripts/amatyaz-1.9.js
@@ -137,7 +137,7 @@ class ExpandingList extends HTMLUListElement {
         });
         $(this).on("click", ".parent1.childs a", function(e) {
             e.preventDefault();
-            $(this).next("ul").slideToggle("slow");
+            $(this).next("ul").slideToggle();
         });
 
         // Get ul and li elements that are a child of this custom ul element
@@ -156,8 +156,6 @@ class ExpandingList extends HTMLUListElement {
             if (li.querySelectorAll("ul").length > 0) {
                 // Add an attribute which can be used  by the style
                 // to show an open or closed icon
-                //li.setAttribute("class", "closed");
-                //debugger;
                 const childText = li.childNodes[0];
 
                 //const newSpan = document.createElement("span");
@@ -165,28 +163,9 @@ class ExpandingList extends HTMLUListElement {
                 if (newSpan.tagName !== "A") {
                     return;
                 }
-                //newSpan.style.cursor = "pointer";
-                //newSpan.innerHTML = `<i class="bi-bookmark"></i>${childText.textContent}`;
-
                 newSpan.addEventListener("click", (e) => {
-                    // next sibling to the span should be the ul
                     $(e.target).next("ul").slideToggle();
-                    /*
-                    const nextul = e.target.nextElementSibling;
-                    const ctrl = this;
-                    // Toggle visible state and update class attribute on ul
-                    if (nextul.style.display == "block") {
-                        nextul.style.display = "none";
-                        nextul.parentNode.setAttribute("class", "closed");
-                    } else {
-                        nextul.style.display = "block";
-                        nextul.parentNode.setAttribute("class", "open");
-                    }
-                        */
                 });
-                // Add the span and remove the bare text node from the li
-                //childText.parentNode.insertBefore(newSpan, childText);
-                //childText.parentNode.removeChild(childText);
             }
         });
     }
