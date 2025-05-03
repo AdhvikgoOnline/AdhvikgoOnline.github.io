0177deb928059ac1524074ad516f4703b89d27c2
Vinayak Prakash Sutar <dvinayaksutar@gmail.com>
2025-05-03 21:19:28 +0530

    scripts/amatyaz-1.0.js

--- scripts/old1.js
+++ scripts/old1.js
@@ -717,19 +717,6 @@ jQuery.cachedScript = function (url, options) {
     // Return the jqXHR object so we can chain callbacks
     return jQuery.ajax(options);
 };
-var urlParams;
-(window.onpopstate = function () {
-    var match,
-        pl = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query = window.location.search.substring(1);
-
-    urlParams = {};
-    while (match = search.exec(query))
-        urlParams[decode(match[1])] = decode(match[2]);
-})();
-
 
 
 
